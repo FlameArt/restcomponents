1563fc8c7f682ab25ba2e7f8bda65dd468db5d18
Flame <flameartme@gmail.com>
2022-08-15 00:32:02 +0300

    feat: namespace for Tables

--- src/restgen/default/tables.php
+++ src/restgen/default/tables.php
@@ -1,3 +1,8 @@
 <?php
 
-echo "<?php return " . str_replace(["array", "(", ")", "\\\\", "\n"], ["","[","]", "\\", "\n"], var_export($arr, true)) . ";";
+echo "<?php namespace common\models\DB\models;
+
+
+class Tables {
+   
+   const all = " . str_replace(["array", "(", ")", "\\\\", "\n"], ["","[","]", "\\", "\n"], var_export($arr, true)) . ";}";
